155727fe9278ed15f0357c89563c5d29f7a7bd4f
Nahuel Moreno <nahuel23009@gmail.com>
2023-08-23 19:06:42 -0300

    Listo para empezar el proyecto final

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAoP3qY-i9IkeHbAlLpwRyiItRVNGKpgBQ",
+  authDomain: "supex-lp.firebaseapp.com",
+  projectId: "supex-lp",
+  storageBucket: "supex-lp.appspot.com",
+  messagingSenderId: "1000362705273",
+  appId: "1:1000362705273:web:4e5dd2a554e8022eb3efa8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
